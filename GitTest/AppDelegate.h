bb63fcc7396531fde18666d7bc2c509942ff2fef
DisNavi <igogalai@gmail.com>
2015-02-18 20:21:57 +0300

    Initial Commit

---
+++ GitTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitTest
+//
+//  Created by Igor on 18.02.15.
+//  Copyright (c) 2015 iGalayDevelopment. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitTest
+//
+//  Created by Igor on 18.02.15.
+//  Copyright (c) 2015 iGalayDevelopment. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
